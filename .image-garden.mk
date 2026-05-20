e9f76e2fc35bfbff6dfbfcf025bb8724f3733b81
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2026-05-20 09:07:34 +0200

    test(snap): pre-pull default image more reliably
    We need to give docker a moment to finish setting up. We an also add a
    docker system group while we are at it. With this setting it is worth to
    keep prepared images around in CI but the time advantage is worth it.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- .image-garden.mk
+++ .image-garden.mk
@@ -3,19 +3,24 @@
 
 default_sandbox_image=ghcr.io/nvidia/openshell-community/sandboxes/base:latest
 
+define install_and_setup_docker
+- addgroup --system docker
+- snap install docker
+- while [ ! -S /run/docker.sock ]; do sleep 3; done
+- "snap run docker pull $(default_sandbox_image)"
+endef
+
 define UBUNTU_CLOUD_INIT_USER_DATA_TEMPLATE
 $(CLOUD_INIT_USER_DATA_TEMPLATE)
 - snap wait system seed.loaded
-- snap install docker
-- snap run docker pull $(default_sandbox_image)
+$(install_and_setup_docker)
 endef
 
 define DEBIAN_CLOUD_INIT_USER_DATA_TEMPLATE
 $(CLOUD_INIT_USER_DATA_TEMPLATE)
 - systemctl enable --now snapd.socket snapd.service snapd.apparmor.service
 - snap wait system seed.loaded
-- snap install docker
-- snap run docker pull $(default_sandbox_image)
+$(install_and_setup_docker)
 packages:
 - snapd
 endef
@@ -26,8 +31,7 @@ $(CLOUD_INIT_USER_DATA_TEMPLATE)
 - systemctl enable --now snapd.socket
 - snap wait system seed.loaded
 - sudo ln -s /var/lib/snapd/snap /snap
-- snap install docker
-- snap run docker pull $(default_sandbox_image)
+$(install_and_setup_docker)
 endef
 
 define CENTOS_CLOUD_INIT_USER_DATA_TEMPLATE
@@ -37,6 +41,5 @@ $(CLOUD_INIT_USER_DATA_TEMPLATE)
 - systemctl enable --now snapd.socket snapd.service
 - snap wait system seed.loaded
 - sudo ln -s /var/lib/snapd/snap /snap
-- snap install docker
-- snap run docker pull $(default_sandbox_image)
+$(install_and_setup_docker)
 endef
