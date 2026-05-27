fcf19aa8627db09e77562233e86d95903c5a1334
Simon Scatton <sscatton@nvidia.com>
2026-05-27 10:25:43 +0200

    feat: add musl target on vm driver and add toolchains to nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,10 @@
     }:
     let
       eachSystem = flake-utils.lib.eachSystem;
-      systems = [ "x86_64-linux" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
     in
     eachSystem systems (
       system:
@@ -37,7 +40,14 @@
           inherit overlays system;
         };
 
-        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          targets = [
+            "aarch64-unknown-linux-musl"
+            "x86_64-unknown-linux-musl"
+          ];
+        };
+        aarch64LinuxMuslCc = pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc;
+        x86_64LinuxMuslCc = pkgs.pkgsCross.musl64.stdenv.cc;
         treefmt = treefmt-nix.lib.evalModule pkgs {
           projectRootFile = "flake.nix";
           programs.nixfmt.enable = true;
@@ -49,6 +59,8 @@
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             rustToolchain
+            aarch64LinuxMuslCc
+            x86_64LinuxMuslCc
 
             # Required for bindgen
             llvmPackages.libclang
@@ -58,6 +70,10 @@
           ];
           env = {
             LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+            CC_aarch64_unknown_linux_musl = "${aarch64LinuxMuslCc}/bin/${aarch64LinuxMuslCc.targetPrefix}cc";
+            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${aarch64LinuxMuslCc}/bin/${aarch64LinuxMuslCc.targetPrefix}cc";
+            CC_x86_64_unknown_linux_musl = "${x86_64LinuxMuslCc}/bin/${x86_64LinuxMuslCc.targetPrefix}cc";
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${x86_64LinuxMuslCc}/bin/${x86_64LinuxMuslCc.targetPrefix}cc";
           };
         };
       }
